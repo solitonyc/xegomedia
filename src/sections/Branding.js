ed48313675f852322f9cd0d7d86dbef96483f632
Solito Reyes II <solitonyc@gmail.com>
2021-02-05 22:57:06 -0500

    branding section design template in place

--- src/sections/Branding.js
+++ src/sections/Branding.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 import './branding.css';
+import mainBgImage from './images-sections/BgMain.jpg';
+import bigLogo from './images-sections/wings-logo.png';
 
 export default class Branding extends Component {
     render() {
+        let style = {
+            backgroundImage: `url(${mainBgImage})`
+        }
         return (
             <div className='branding_section' id='branding'>
-                <h1>This is the BRANDING section</h1>
+                <div style={style} className='branding_section_background' >
+                    <div className='branding_content_container'>
+                        <img className='branding_logo' alt='' src={bigLogo} />  
+                        <div className='branding_headline'>BRANDING SECTION HEADLINE</div>
+                        <div className='branding_headline_2'>This is the secondary headline</div>
+                        <div className='branding_copy'>Main copy liquip commodo consectetur nisi esse nulla irure duis laboris.</div>
+                    </div>
+                </div>
             </div>
         )
     }
