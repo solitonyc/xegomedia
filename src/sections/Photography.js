26e5a75b5625b6d5fa8f50a3e49dc4e2195cf791
Solito Reyes II <solitonyc@gmail.com>
2021-02-07 16:29:03 -0500

    new headline for photo section

--- src/sections/Photography.js
+++ src/sections/Photography.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './photography.css';
 import mainBgImage from './images-sections/BgMain.jpg';
-import bigLogo from './images-sections/wings-logo.png';
+
 
 export default class Photography extends Component {
     render() {
@@ -11,9 +11,8 @@ export default class Photography extends Component {
         return (
             <div className='photography_section' id='photography'>
                 <div style={style} className='photography_section_background' >
-                    <div className='photography_content_container'>
-                        <img className='photography_logo' alt='' src={bigLogo} />  
-                        <div className='photography_headline'>PHOTOGRAPHY SECTION HEADLINE</div>
+                    <div className='photography_content_container'>                        
+                        <div className='photography_headline'>CAPTURE MOMENTS VIVIDLY</div>
                         <div className='photography_headline_2'>This is the secondary headline</div>
                         <div className='photography_copy'>Main copy liquip commodo consectetur nisi esse nulla irure duis laboris.</div>
                     </div>
