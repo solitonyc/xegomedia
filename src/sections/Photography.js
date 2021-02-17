77e8565c84e996633c6c0ef189fde3b5d0f15a43
Solito Reyes II <solitonyc@gmail.com>
2021-02-17 00:16:12 -0500

    added router link to solitophotography.com

--- src/sections/Photography.js
+++ src/sections/Photography.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import { BrowserRouter as Router, Switch } from "react-router-dom";
 import './photography.css';
 import mainBgImage from './images-sections/bg-17.jpg';
 
@@ -15,6 +17,9 @@ export default class Photography extends Component {
                         <div className='photography_headline'>PHOTOGRAPHY</div>
                         <div className='photography_headline_2'>Capture Lasting Moments</div>
                         <div className='photography_copy'>Shape your BRAND using captivating photos and accent imagery.</div>
+                        <Router>
+                            <Link to={{ pathname: 'https://solitophotography.com' }} rel='noreferrer' className='common_button' target="_blank" >See Photography</Link>
+                        </Router>
                     </div>
                 </div>
             </div>
